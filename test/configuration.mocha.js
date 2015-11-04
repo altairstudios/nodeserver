88242cac6ad6dd1c15f278b992395d4c3e591449
Juan Benavides <juan@altairstudios.es>
2015-11-04 01:42:58 +0100

    Add some configs

--- test/configuration.mocha.js
+++ test/configuration.mocha.js
@@ -16,7 +16,8 @@ describe('Configurations checks', function(){
 	});
 
 
-	it('malformed configuration', function(done){
+
+	it('malformed configuration from file', function(done){
 		var nodeserver = new require('../nodeserver')(false);
 		nodeserver.readConfigFile('./configurations/malformed.config');
 		nodeserver.start();
@@ -28,7 +29,8 @@ describe('Configurations checks', function(){
 	});
 
 
-	it('empty configuration', function(done){
+
+	it('empty configuration from file', function(done){
 		var nodeserver = new require('../nodeserver')(false);
 		nodeserver.readConfigFile('./configurations/empty.config');
 		nodeserver.start();
@@ -40,9 +42,36 @@ describe('Configurations checks', function(){
 	});
 
 
-	it('string properties configuration', function(done){
+
+	it('empty configuration from json', function(done){
+		var nodeserver = new require('../nodeserver')(false);
+		nodeserver.readConfig({});
+		nodeserver.start();
+
+		assert.equal(0, nodeserver.websites.length, 'The number of web pages should be 0');
+		assert.equal(undefined, nodeserver.admin, 'The admin should be undefined');
+
+		done();
+	});
+
+
+
+	it('string properties configuration from file', function(done){
 		var nodeserver = new require('../nodeserver')(false);
 		nodeserver.readConfigFile('./configurations/string-properties.config');
+		nodeserver.start();
+
+		assert.equal(0, nodeserver.websites.length, 'The number of web pages should be 0');
+		assert.equal(undefined, nodeserver.admin, 'The admin should be undefined');
+
+		done();
+	});
+
+
+
+	it('string properties configuration from json', function(done){
+		var nodeserver = new require('../nodeserver')(false);
+		nodeserver.readConfig({ "nodeserver": "This need throw an error", "sites": "This need throw an error" });
 		nodeserver.start();
 
 		assert.equal(0, nodeserver.websites.length, 'The number of web pages should be 0');
